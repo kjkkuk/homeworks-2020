a48fbb695437456eb4daefdaa41ce6218b027d08
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-03-22 03:35:53 +0300

    upd task02 by rubocop

--- HW01/Yakutovich_Dima/dima_yakutovich_hw_01_t_02.rb
+++ HW01/Yakutovich_Dima/dima_yakutovich_hw_01_t_02.rb
@@ -1,10 +1,9 @@
-#this code shows simple calculations in prog
+# this code shows simple calculations in prog
 puts "'1/3 + 4 * 6 = 24'--?"
 
-
-if 24 == 1/3 + 4*6
-  puts 1/3+4*6
+if 1 / 3 + 4 * 6 == 24
+  puts 1 / 3 + 4 * 6
   p "it's shows that is true"
 else
-  "u have an error :)"
+  'u have an error :)'
 end
