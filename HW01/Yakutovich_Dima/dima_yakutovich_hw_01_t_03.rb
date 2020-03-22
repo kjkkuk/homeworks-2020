1a88654b8445f6a8a3fbcf2f983ea1c780d7c6b7
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-03-22 03:13:38 +0300

    upd by rubocop

--- HW01/Yakutovich_Dima/dima_yakutovich_hw_01_t_03.rb
+++ HW01/Yakutovich_Dima/dima_yakutovich_hw_01_t_03.rb
@@ -1,6 +1,6 @@
-if 2009 == 2222 - 222 + 22/2 - 2
-  puts 2222-222+22/2-2
-  p "this solution is true"
+if 2222 - 222 + 22 / 2 - 2 == 2009
+  puts 2222 - 222 + 22 / 2 - 2
+  p 'this solution is true'
 else
-  "u have an error :)"
+  'u have an error :)'
 end
