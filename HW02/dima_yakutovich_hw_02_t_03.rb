5a3f57dc9e11a3de94e46ab89c9cb4d968fb12ce
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-03-29 15:09:05 +0300

    create def for parsing time

--- HW02/dima_yakutovich_hw_02_t_03.rb
+++ HW02/dima_yakutovich_hw_02_t_03.rb
@@ -2,3 +2,7 @@ require 'time'
 #############
 TIME_REGEX = /^\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2}\.\d{1}/.freeze
 debugs = File.readlines('input_task03.txt') # logs information
+
+def parsing(time)
+  Time.parse(time[TIME_REGEX])
+end
