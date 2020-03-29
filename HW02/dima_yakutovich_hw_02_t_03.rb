27875019331189005754f06847dc8c39370808f4
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-03-29 15:10:51 +0300

    create def with valid and checking debugs

--- HW02/dima_yakutovich_hw_02_t_03.rb
+++ HW02/dima_yakutovich_hw_02_t_03.rb
@@ -3,6 +3,12 @@ require 'time'
 TIME_REGEX = /^\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2}\.\d{1}/.freeze
 debugs = File.readlines('input_task03.txt') # logs information
 
+def get_time(logs)
+  events = logs.select { |line| line.downcase.include? 'calling core' }
+  0 if events.size < 2
+
+end
+
 def parsing(time)
   Time.parse(time[TIME_REGEX])
 end
