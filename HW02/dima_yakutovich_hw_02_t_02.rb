aaccf4a450c701a1421db5620f8b5b4c3b0bfe34
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-03-29 15:05:26 +0300

    task2: rubocop -a

--- HW02/dima_yakutovich_hw_02_t_02.rb
+++ HW02/dima_yakutovich_hw_02_t_02.rb
@@ -1,11 +1,13 @@
-logs = File.readlines('logs.txt') # logs information
-def logs_form(edited_logs)
+if File.exist? 'edited_logs.txt'
+  logs = File.readlines('logs.txt') # logs information
+
+  def logs_form(edited_logs)
     edited_logs.select { |lines| lines.include? 'POST' }.map do |str|
       ip = str.match(/([0-9]{1,3}[\.]){3}[0-9]{1,3}/)
       date = str.match(%r{(\w+/\w+/\w+.+\+\w+)})
       message = str.match(/POST\s\S+/).to_s.gsub(/POST /, '').upcase
       "#{date} FROM: #{ip} TO: #{message}"
-    end 
+    end
   end
 
   File.new('edited_logs.txt', 'w')
@@ -15,3 +17,4 @@ def logs_form(edited_logs)
       logs_form(logs).each { |lines| file.puts(lines) }
     end
   end
+end
