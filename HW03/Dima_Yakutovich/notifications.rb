a3f15bb63e525548c6358d6948a7605a823ac5a0
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-04-02 21:49:36 +0300

    upd notification class

--- HW03/Dima_Yakutovich/notifications.rb
+++ HW03/Dima_Yakutovich/notifications.rb
@@ -15,6 +15,18 @@ class Notification
     end
   end
 
+  def student_str
+    "#{homework.student.name} #{homework.student.surname}"
+  end
+
+  def student_mentors
+    homework.student.mentors
+  end
+
+  def student_notifications
+    homework.student.notifications
+  end
+
   def add_new_lection
     notify(receiver: :student, message: "Add new lection #{lections.title}")
   end
@@ -27,12 +39,12 @@ class Notification
     notify(receiver: :mentor, message: "Mentor create: #{homework.title}")
   end
 
-  def questions; end
+  def questions
+    notify(receiver: :mentor, message: "Student's questions: #{questions}")
+  end
 
-  def answers; end
-
-  def homework_to_work
-    notify(receiver: :mentor, message: "Student #{student_str} do: #{homework.title}")
+  def answers
+    notify(receiver: :student, message: "Mentor's answers: #{answers}")
   end
 
   def homework_to_check
@@ -43,9 +55,9 @@ class Notification
     notify(receiver: :student, message: "Your homework '#{homework.title}' completed!")
   end
 
-  def error_homework
-    notify(receiver: :student, message: "Your homework '#{homework.title}' have an errors!")
-  end
+  # def error_homework
+  #  notify(receiver: :student, message: "Your homework '#{homework.title}' have an errors!")
+  # end
 
   def send_student(message)
     student_notifications << message
@@ -55,17 +67,5 @@ class Notification
     student_mentors.each do |mentor|
       mentor.notifications << message
     end
-  end
-
-  def student_str
-    "#{homework.student.name} #{homework.student.surname}"
-  end
-
-  def student_mentors
-    homework.student.mentors
-  end
-
-  def student_notifications
-    homework.student.notifications
   end
 end
