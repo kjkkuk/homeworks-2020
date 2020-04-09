4df1bc4c58f5bdc8da9b507a3d9fa9fc72fc9d20
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-04-10 02:45:04 +0300

    Update main_methods.rb

--- HW04/Dima_Yakutovich/main_methods.rb
+++ HW04/Dima_Yakutovich/main_methods.rb
@@ -5,7 +5,6 @@ class Array
   def my_each
     if block_given?
       i = 0
-
       while i < length
         yield (self[i])
         i += 1
@@ -13,22 +12,19 @@ class Array
     else to_enum
     end
   end
-  
+
   def my_map
     output = []
-
     if block_given?
-
       my_each { |i| output << yield(i) }
     else to_enum
     end
     output
   end
+
   def my_select
     if block_given?
-
       output = []
-
       my_each do |i|
         output << i if yield(i)
       end
