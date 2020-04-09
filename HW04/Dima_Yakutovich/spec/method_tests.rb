aff97df22683d3ea4d357dddfca9e974b6877334
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-04-09 18:10:34 +0300

    add test of select

--- HW04/Dima_Yakutovich/spec/method_tests.rb
+++ HW04/Dima_Yakutovich/spec/method_tests.rb
@@ -12,3 +12,15 @@ RSpec.describe Array do
     end
   end
 end
+
+RSpec.describe Array do
+  subject { Array.new([1, 2, 3, 4, 5, 'abcd', 256]) }
+  describe '#my_select' do
+    context 'when default select' do
+      it 'should return instance of integer into array with select' do
+        expect(subject.my_select { |x| x.instance_of?(Integer) })
+          .to eq [3, 2, 1, 4, 5, 256]
+      end
+    end
+  end
+end
