be946993061f102d09348529b96fc8a2ce5c4537
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-04-09 18:10:09 +0300

    add test of map

--- HW04/Dima_Yakutovich/spec/method_tests.rb
+++ HW04/Dima_Yakutovich/spec/method_tests.rb
@@ -1,2 +1,14 @@
 require_relative '../main_methods.rb'
 require 'rspec'
+
+RSpec.describe Array do
+  subject { Array.new([1, 2, 3, 4, 5, 'abcd', 256]) }
+  describe '#my_map' do
+    context 'when default map' do
+      it 'should return upd array' do
+        expect(subject.my_map { |i| i * 2 })
+          .to eq [2, 4, 6, 8, 10, 'abcdabcd', 512]
+      end
+    end
+  end
+end
