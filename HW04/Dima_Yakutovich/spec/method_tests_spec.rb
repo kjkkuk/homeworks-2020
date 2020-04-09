95864151558b3bc660b68389598ae7e6568d067f
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-04-10 02:15:16 +0300

    Update and rename method_tests.rb to method_tests_spec.rb

--- HW04/Dima_Yakutovich/spec/method_tests_spec.rb
+++ HW04/Dima_Yakutovich/spec/method_tests_spec.rb
@@ -1,31 +1,27 @@
+# rubocop:disable Metrics/BlockLength
+
 require_relative '../main_methods.rb'
 require 'rspec'
 
 RSpec.describe Array do
-  subject { Array.new([1, 2, 3, 4, 5, 'abcd', 256]) }
+  subject { Array.new([1, 2, 3, 4, 5, 256]) }
   describe '#my_map' do
     context 'when default map' do
       it 'should return upd array' do
         expect(subject.my_map { |i| i * 2 })
-          .to eq [2, 4, 6, 8, 10, 'abcdabcd', 512]
+          .to eq [2, 4, 6, 8, 10, 512]
       end
     end
   end
-end
-
-RSpec.describe Array do
-  subject { Array.new([1, 2, 3, 4, 5, 'abcd', 256]) }
+  subject { Array.new([1, 2, 3, 4, 5, 256, 'abcd']) }
   describe '#my_select' do
     context 'when default select' do
       it 'should return instance of integer into array with select' do
         expect(subject.my_select { |x| x.instance_of?(Integer) })
-          .to eq [3, 2, 1, 4, 5, 256]
+          .to eq [1, 2, 3, 4, 5, 256]
       end
     end
   end
-end
-
-RSpec.describe Array do
   subject { Array.new([1, 2, 3, 4, 5, 256]) }
   describe '#my_each' do
     context 'when def each' do
@@ -33,7 +29,7 @@ RSpec.describe Array do
         expectation = expect do
           (subject.my_each { |i| puts i * 2 })
         end
-        expectation.to output("2\n4\n6\n8\n10\n512").to_stdout
+        expectation.to output("2\n4\n6\n8\n10\n512\n").to_stdout
       end
     end
   end
