c188723a4e948ec29d3a89a6ee532debefdf2623
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-04-13 16:55:32 +0300

    Update and rename method_tests_spec.rb to tests_array_spec.rb

--- HW04/Dima_Yakutovich/spec/tests_array_spec.rb
+++ HW04/Dima_Yakutovich/spec/tests_array_spec.rb
@@ -1,33 +1,34 @@
 # frozen_string_literal: true
 
-require_relative '../main_methods.rb'
+require_relative '../array_methods.rb'
 require 'rspec'
 
 RSpec.describe Array do
-  subject { Array.new([1, 2, 3, 4, 5, 256]) }
+  subject(:data) { described_class.new([1, 2, 3, 4, 5, 256]) }
+
   describe '#my_map' do
     context 'when default map' do
-      it 'should return upd array' do
-        expect(subject.my_map { |i| i * 2 })
+      it 'returns upd array' do
+        expect(data.my_map { |i| i * 2 })
           .to eq [2, 4, 6, 8, 10, 512]
       end
     end
   end
-  subject { Array.new([1, 2, 3, 4, 5, 256, 'abcd']) }
+
   describe '#my_select' do
     context 'when default select' do
-      it 'should return instance of integer into array with select' do
-        expect(subject.my_select { |x| x.instance_of?(Integer) })
+      it 'returns instance of integer into array with select' do
+        expect(data.my_select { |x| x.instance_of?(Integer) })
           .to eq [1, 2, 3, 4, 5, 256]
       end
     end
   end
-  subject { Array.new([1, 2, 3, 4, 5, 256]) }
+
   describe '#my_each' do
     context 'when def each' do
-      it 'should return column of output' do
+      it 'returns column of output' do
         expectation = expect do
-          (subject.my_each { |i| puts i * 2 })
+          (data.my_each { |i| puts i * 2 })
         end
         expectation.to output("2\n4\n6\n8\n10\n512\n").to_stdout
       end
