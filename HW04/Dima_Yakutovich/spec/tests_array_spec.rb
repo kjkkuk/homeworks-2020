7217c754e35e6ac794c120735b181e7e97d9021f
Dmitry Yakutovich <35342962+kjkkuk@users.noreply.github.com>
2020-05-04 12:29:07 +0300

    Update tests_array_spec.rb

--- HW04/Dima_Yakutovich/spec/tests_array_spec.rb
+++ HW04/Dima_Yakutovich/spec/tests_array_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe Array do
   subject(:data) { described_class.new([1, 2, 3, 4, 5, 256]) }
 
   describe '#my_map' do
-    context 'when default map' do
-      it 'returns upd array' do
+    context 'when block is given' do
+      it 'returns values as the default map' do
         expect(data.my_map { |i| i * 2 })
           .to eq [2, 4, 6, 8, 10, 512]
       end
@@ -16,8 +16,8 @@ RSpec.describe Array do
   end
 
   describe '#my_select' do
-    context 'when default select' do
-      it 'returns instance of integer into array with select' do
+    context 'when block is given' do
+      it 'returns outputs eq with select' do
         expect(data.my_select { |x| x.instance_of?(Integer) })
           .to eq [1, 2, 3, 4, 5, 256]
       end
@@ -25,8 +25,8 @@ RSpec.describe Array do
   end
 
   describe '#my_each' do
-    context 'when def each' do
-      it 'returns column of output' do
+    context 'when block is given' do
+      it 'returns output as the default each' do
         expectation = expect do
           (data.my_each { |i| puts i * 2 })
         end
